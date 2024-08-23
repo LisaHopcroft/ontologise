8b6f25a956472e592c8925a010bf5b0b0b608b95
LisaHopcroft <LisaEMHopcroft+github@gmail.com>
2024-08-23 23:50:57 +0100

    Adding code to parse the source header line (can handle multiple sources in the one document). Tests included.

---
+++ code/bin/utils.py
@@ -0,0 +1,31 @@
+import re
+
+class Document:
+    def __init__(self, file=""):
+
+        self.file = file
+
+        ### Information about the sources
+        self.source_list = []
+
+    def read_document(self):
+        with open(self.file, "r") as d:
+            for l in d:
+                self.scan_for_source_header(l)
+                print( f"--> LINE: {l}\n" )
+
+    def scan_for_source_header(self, l):
+        if l.startswith("#["):
+            m = re.search(r"\[(.*?)\]", l)
+            print( "Srouce header:" + m.group(1) )
+            self.source_list.append( m.group(1) )
+
+    def print_summary(self):
+        print(f"Document parsed = {self.file}")
+        print(f"Contained {len(self.source_list)} sources")
+
+        for order, source_name in enumerate(self.source_list):
+            print(f"[{order+1:02}]: {source_name}")
+
+    def get_source_list(self):
+        return self.source_list
