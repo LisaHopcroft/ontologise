8ef2944a6edd141d8988550b1b7c2d511b1d3a69
LisaHopcroft <LisaEMHopcroft+github@gmail.com>
2024-08-26 20:40:25 +0100

    Adding linting/formatting checks to build_notes.sh

--- build_notes.sh
+++ build_notes.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-### Run tests
+# Run black
+black src
+
+# Run flake8
+flake8 src --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 src --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+
+# Run ruff
+ruff check src --output-format=github
+
+# Run tests
 pytest --rootdir=tests
 
-### Check coverage
+# Check coverage
 pytest --rootdir=tests --cov=src
 
-### Generate requirements files
+# Generate requirements files
 pipreqs --savepath=requirements.in src/ && pip-compile --strip-extras
 
-### Test build
+# Test build
 python3 -m build
 
-### Push to PyPI
+# Push to PyPI
 twine upload dist/ontologise-[...]
 
-### Generate documentation
+# Generate documentation
 sphinx-apidoc -a -o docs/source src/ontologise tests
 cd docs
 make clean; make html
