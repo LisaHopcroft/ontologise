9be8dc1252fdb9fe3562891504d57b3522126e56
LisaHopcroft <LisaEMHopcroft+github@gmail.com>
2024-10-17 15:20:47 +0100

    Updating build notes for documentation generation

--- build_notes.sh
+++ build_notes.sh
@@ -2,13 +2,17 @@
 
 # Run black
 black src
+black tests
 
 # Run flake8
 flake8 src --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 tests --count --select=E9,F63,F7,F82 --show-source --statistics
 flake8 src --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+flake8 tests --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
 
 # Run ruff
 ruff check src --output-format=github
+ruff check tests --output-format=github
 
 # Run tests
 pytest --rootdir=tests
@@ -30,6 +34,13 @@ python3 -m build
 twine upload dist/ontologise-[...]
 
 # Generate documentation
+rm docs/source/utils.rst
+rm docs/source/modules.rst
+# Remove any obsolete .rst/.html files, e.g.:
+# rm docs/source/test_parser.rst
+# rm docs/html/test_parser.html
+
 sphinx-apidoc -a -o docs/source src/ontologise tests
-cd docs
-make clean; make html
+sphinx-build -a -b html docs/source docs/html -j auto
+# cd docs
+# make clean; make html
