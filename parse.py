8979de0560ed8d6b02ab0f35349a025d253bf0ff
LisaHopcroft <LisaEMHopcroft+github@gmail.com>
2025-01-10 14:24:07 +0000

    Adding command line options

--- parse.py
+++ parse.py
@@ -1,14 +1,71 @@
 from src.ontologise.utils import Document
+import argparse
+from argparse import ArgumentParser
+import os.path
 
-# file_to_parse = "data/p2833, Felicity with new format sample.txt"
-# d = Document(file_to_parse)
+### Defining the default settings file
+default_settings_file = "settings.yaml"
 
-file_to_parse = "data/p2833, Felicity with new format multiple shortcuts stop_at_global_id.txt"
-d = Document(file_to_parse, settings_file="settings2.yaml")
+### Checking that a file is valid
+def is_valid_file(parser, arg):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return open(arg, "r")  # return an open file handle
 
-# file_to_parse = "data/p2833, Felicity.txt"
-# d = Document(file_to_parse, settings_file="settings.yaml")
+### Parsing the command line argument
+parser = argparse.ArgumentParser(description="Parsing Ontologise markup documents")
 
+### This argument provides the marked up Ontologise document
+### This is REQUIRED
+parser.add_argument(
+    "-i",
+    dest="file",
+    required=True,
+    help="A file using the Ontologise markup format",
+    metavar="FILE",
+    type=lambda x: is_valid_file(parser, x),
+)
+
+### This argument provides the settings file
+### If this argument is not provided, the default settings file (settings.yaml) is used
+parser.add_argument(
+    "-s",
+    dest="settings_yaml",
+    required=False,
+    help="The settings for the parsing",
+    metavar="SETTINGS YAML",
+    type=lambda x: is_valid_file(parser, x),
+)
+
+args = parser.parse_args()
+
+file_to_read = args.file.name
+settings_to_use = None
+
+if args.settings_yaml is None:
+    print( "No value provided for the settings file, using the default settings file" )
+    settings_to_use = default_settings_file
+else:
+    settings_to_use = args.settings_yaml.name
+
+print( f"File to read: {file_to_read}" )
+print( f"Settings to use: {settings_to_use}" )
+
+d = Document(file_to_read, settings_file=settings_to_use)
 
 d.read_document()
 d.print_summary()
+
+# # file_to_parse = "data/p2833, Felicity with new format sample.txt"
+# # d = Document(file_to_parse)
+
+# # file_to_parse = "data/p2833, Felicity with new format multiple shortcuts stop_at_global_id.txt"
+# # d = Document(file_to_parse, settings_file="settings2.yaml")
+
+# # file_to_parse = "data/p2833, Felicity.txt"
+# # d = Document(file_to_parse, settings_file="settings.yaml")
+
+
+# # d.read_document()
+# # d.print_summary()
