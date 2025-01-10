b974abfb39558745fe0e5145da0ef24b150e4164
LisaHopcroft <LisaEMHopcroft+github@gmail.com>
2025-01-10 14:49:51 +0000

    Using logger and adding comments

--- parse.py
+++ parse.py
@@ -1,12 +1,13 @@
-from src.ontologise.utils import Document, DEFAULT_SETTINGS
+from src.ontologise.utils import Document, DEFAULT_SETTINGS, logger
 import argparse
 from argparse import ArgumentParser
 import os.path
+import sys
 
 ### Checking that a file is valid
 def is_valid_file(parser, arg):
     if not os.path.exists(arg):
-        parser.error("The file %s does not exist!" % arg)
+        logger.critical("The file '%s' does not exist!" % arg)
     else:
         return open(arg, "r")  # return an open file handle
 
@@ -35,21 +36,32 @@ parser.add_argument(
     type=lambda x: is_valid_file(parser, x),
 )
 
+### Parse the arguments
 args = parser.parse_args()
+logger.debug(args)
 
+### Stop processing if the file name is missing
+if ( args.file is None):
+    sys.exit( 1 )
+
+### If the file name is present, use it
 file_to_read = args.file.name
+
+### If the settings file is missing, use the default one
 settings_to_use = None
 
 if args.settings_yaml is None:
-    print( "No value provided for the settings file, using the default settings file" )
+    logger.info( "No value provided for the settings file, using the default settings file" )
     settings_to_use = DEFAULT_SETTINGS
 else:
     settings_to_use = args.settings_yaml.name
 
-print( f"File to read: {file_to_read}" )
-print( f"Settings to use: {settings_to_use}" )
+logger.debug(f"File to read: {file_to_read}")
+logger.debug(f"Settings to use: {settings_to_use}")
 
+### Do the parsing
 d = Document(file_to_read, settings_file=settings_to_use)
-
 d.read_document()
+
+### Print a summary of the results
 d.print_summary()
